19c2518a567c6236a2f5b9744e43c747bf0793ea
Anirbandasjoy <joy600508@gmail.com>
2024-06-03 01:12:09 +0600

    create admin notice notification model

---
+++ models/adminNoticeNotification.model.ts
@@ -0,0 +1,31 @@
+import { model, Schema } from "mongoose";
+
+const AdminNoticenotificationSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  read: {
+    type: Boolean,
+    default: false,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  notice: {
+    type: Schema.Types.ObjectId,
+    ref: "Notice",
+  },
+});
+
+const AdminNoticeNotification = model(
+  "AdminNoticeNotification",
+  AdminNoticenotificationSchema
+);
+
+export default AdminNoticeNotification;

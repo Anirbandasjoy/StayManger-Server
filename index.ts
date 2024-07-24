1bd44110dd5870f739a9191851bdc4457cee9c4d
Anirbandasjoy <joy600508@gmail.com>
2024-07-25 01:29:04 +0600

    production db url added

--- index.ts
+++ index.ts
@@ -1,9 +1,18 @@
 import app from "./app";
+import http from "http";
 import dbConnection from "./config/db";
-
 const PORT = process.env.PORT || 5000;
+const server = http.createServer(app);
 
-app.listen(PORT, async () => {
-  console.log(`Server is running  http://localhost:${PORT}`);
-  await dbConnection();
-});
+const main = async () => {
+  try {
+    await dbConnection();
+    server.listen(PORT, async () => {
+      console.log(`Server is running at http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("db error", error);
+  }
+};
+
+main();

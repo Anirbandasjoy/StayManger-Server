e5cc641dc9b3d5b19ae522e342fbf78b96bab4c5
Anirbandasjoy <joy600508@gmail.com>
2024-08-09 21:00:28 +0600

    update

--- index.ts
+++ index.ts
@@ -6,11 +6,15 @@ const server = http.createServer(app);
 const main = async () => {
   try {
     await dbConnection();
-    server.listen(PORT, async () => {
+    server.listen(PORT, () => {
       console.log(`Server is running at http://localhost:${PORT}`);
     });
   } catch (error) {
-    console.error("db error", error);
+    console.error(
+      "Failed to start the server due to a DB connection error",
+      error
+    );
+    process.exit(1);
   }
 };
 
